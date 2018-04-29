bc6ce77967eafccb1dca3713e368721047be5aea
Andrew Huth <ahuth@mavenlink.com>
2018-04-28 18:49:25 -0700

    Style the page a bit

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,30 @@
 import Head from 'next/head'
 
+const styles = {
+  main: {
+    fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    fontSize: 16,
+    lineHeight: 1.5,
+  },
+  item: {
+    listStyle: 'none',
+  }
+}
+
 export default () =>
   <div>
     <Head>
       <title>Andrew Huth</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <main>
+    <main style={styles.main}>
       <ul>
-        <li><a href="https://ahuth.github.io/raycast">Raycast</a></li>
-        <li><a href="https://ahuth.github.io/waves">Waves</a></li>
-        <li><a href="https://ahuth.github.io/frostbite">Frostbite</a></li>
-        <li><a href="https://ahuth.github.io/orbital">Orbital</a></li>
-        <li><a href="https://ahuth.github.io/conway">Conway</a></li>
-        <li><a href="https://github.com/ahuth/standfast">Standfast</a></li>
+        <li style={styles.item}><a href="https://ahuth.github.io/raycast">Raycast</a></li>
+        <li style={styles.item}><a href="https://ahuth.github.io/waves">Waves</a></li>
+        <li style={styles.item}><a href="https://ahuth.github.io/frostbite">Frostbite</a></li>
+        <li style={styles.item}><a href="https://ahuth.github.io/orbital">Orbital</a></li>
+        <li style={styles.item}><a href="https://ahuth.github.io/conway">Conway</a></li>
+        <li style={styles.item}><a href="https://github.com/ahuth/standfast">Standfast</a></li>
       </ul>
     </main>
   </div>
