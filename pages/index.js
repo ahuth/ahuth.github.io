e6a96ad9c6ef68e4f0d0865488d5dec03cef69de
Andrew Huth <ahuth@mavenlink.com>
2018-04-28 18:22:40 -0700

    Add some basic links to projects

--- pages/index.js
+++ pages/index.js
@@ -6,5 +6,14 @@ export default () =>
       <title>Andrew Huth</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <p>Hello world!</p>
+    <main>
+      <ul>
+        <li><a href="https://ahuth.github.io/raycast">Raycast</a></li>
+        <li><a href="https://ahuth.github.io/waves">Waves</a></li>
+        <li><a href="https://ahuth.github.io/frostbite">Frostbite</a></li>
+        <li><a href="https://ahuth.github.io/orbital">Orbital</a></li>
+        <li><a href="https://ahuth.github.io/conway">Conway</a></li>
+        <li><a href="https://github.com/ahuth/standfast">Standfast</a></li>
+      </ul>
+    </main>
   </div>
