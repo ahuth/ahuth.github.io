0e67d8c58abc84abe48a6ea0b06a4d244d570f4b
Andrew Huth <andrew@huth.me>
2019-06-21 17:10:53 +0200

    Add @ahuth/styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets');
+  eleventyConfig.addPassthroughCopy('node_modules/@ahuth/styles/index.css');
 
   return {
     dir: {
