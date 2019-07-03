cd8b6abd843daee383d1f4cc956bc04fc873923a
Andrew Huth <andrew@huth.me>
2019-07-02 21:19:11 -0700

    Add syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/.nojekyll');
   eleventyConfig.addPassthroughCopy('src/assets');
   eleventyConfig.addPassthroughCopy('node_modules/@ahuth/styles/index.css');
+  eleventyConfig.addPassthroughCopy('node_modules/prism-themes/themes/prism-atom-dark.css');
+
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
     dir: {
