3493ebd6e66cb7379a67f84f6e64c900da5892d6
Andrew Huth <andrew@huth.me>
2019-07-08 21:23:17 -0700

    Add purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-json',
+    'gatsby-plugin-purgecss',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
