b70b492f732372bcf88823c58ab448f784066bd1
Andrew Huth <andrew@huth.me>
2019-11-28 19:21:01 -0800

    Process html files

--- scripts/build/index.ts
+++ scripts/build/index.ts
@@ -1,6 +1,25 @@
+import fs from 'fs';
+import glob from 'glob';
 import nunjucks from 'nunjucks';
 
 nunjucks.configure('src');
 
-const result = nunjucks.render('pages/index.html');
-console.log(result);
+const pageFiles = glob.sync('src/pages/**/*.html', { nodir: true });
+
+const results = pageFiles.map(function (filePath) {
+  const localizedPath = filePath.replace(/^src\//, '');
+  const outputPath = filePath.replace(/^src\/pages\//, 'build/');
+  const contents = nunjucks.render(localizedPath);
+  return {
+    contents,
+    outputPath,
+  };
+});
+
+if (!fs.existsSync('build/')) {
+  fs.mkdirSync('build/');
+}
+
+results.forEach(function (result) {
+  fs.writeFileSync(result.outputPath, result.contents);
+});
