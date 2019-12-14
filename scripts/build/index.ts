1c890a7ce92607341ffb4ddb51a155acb278a361
Andrew Huth <andrew@huth.me>
2019-12-14 12:32:48 -0800

    Directly glob for article files
    Instead of all markdown ones.

--- scripts/build/index.ts
+++ scripts/build/index.ts
@@ -5,14 +5,14 @@ import * as Output from './output';
 import * as Page from './page';
 
 const pageFiles = glob.sync('src/pages/**/*.html', { nodir: true });
-const markdownFiles = glob.sync('src/pages/**/*.md', { nodir: true });
+const articleFiles = glob.sync('src/pages/articles/**/*.md', { nodir: true });
 
 // Create the build directory if necessary.
 execSync('mkdir -p build');
 
 // Read each markdown article file, render the markdown into a nunjucks template, and write the
 // file to the build directory.
-const articles = markdownFiles.map(Article.read);
+const articles = articleFiles.map(Article.read);
 const articleResults = articles.map(Output.fromArticle);
 articleResults.forEach(Output.write);
 
