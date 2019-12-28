fed3247df37746d3d3f1412e76835c87f9bba09d
Andrew Huth <andrew@huth.me>
2019-12-28 09:00:41 -0800

    Tweak naming

--- scripts/build/index.ts
+++ scripts/build/index.ts
@@ -4,20 +4,19 @@ import * as Article from './article';
 import * as Output from './output';
 import * as Page from './page';
 
-const pageFiles = glob.sync('src/pages/**/*.html', { nodir: true });
-const articleFiles = glob.sync('src/articles/**/*.md', { nodir: true });
+const pageFilePaths = glob.sync('src/pages/**/*.html', { nodir: true });
+const articleFilePaths = glob.sync('src/articles/**/*.md', { nodir: true });
 
 // Create the build directory if necessary.
 execSync('mkdir -p build');
 
-// Read each markdown article file, render the markdown into a nunjucks template, and write the
-// file to the build directory.
-const articles = articleFiles.map(Article.read);
+// Read each markdown article file, convert to html, and write to the build directory.
+const articles = articleFilePaths.map(Article.read);
 const articleOutput = articles.map(Output.fromArticle);
 articleOutput.forEach(Output.write);
 
 // Read each html page and write to the build directory.
-const pages = pageFiles.map(Page.read);
+const pages = pageFilePaths.map(Page.read);
 const pageOutput = pages.map(page => Output.fromPage(page, articles));
 pageOutput.forEach(Output.write);
 
