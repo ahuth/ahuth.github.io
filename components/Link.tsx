afe18d6f0a746175e38742e9592a64eaf4619094
Andrew Huth <git@huth.me>
2023-11-29 17:18:41 -0500

    Randomize link color

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,12 +1,25 @@
 import clsx from 'clsx';
+import sample from 'lodash/sample';
 import type { ComponentProps } from 'react';
+
+const colors = [
+  'text-orange-300',
+  'text-lime-300',
+  'text-teal-300',
+  'text-cyan-300',
+  'text-sky-300',
+  'text-blue-300',
+  'text-fuschia-300',
+  'text-indigo-300',
+  'text-pink-300',
+];
 
 type Props = ComponentProps<'a'>;
 
 export default function Link({children, className, ...props}: Props) {
   return (
     <a
-      className={clsx('text-cyan-300 underline', className)}
+      className={clsx(sample(colors), 'underline', className)}
       {...props}
     >
         {children}
