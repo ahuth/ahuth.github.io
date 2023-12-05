796537afe70f80851ccaba8d8335e244674affbe
Andrew Huth <git@huth.me>
2023-12-05 08:14:34 -0500

    Tweak link colors

--- components/Link.tsx
+++ components/Link.tsx
@@ -2,22 +2,29 @@ import clsx from "clsx";
 import type {ComponentProps} from "react";
 
 const colors = [
-  // "text-red-300",
-  "text-orange-300",
-  "text-amber-300",
-  // "text-yellow-300",
-  "text-lime-300",
-  // "text-green-300",
-  "text-emerald-300",
-  // "text-teal-300",
-  "text-cyan-300",
-  // "text-sky-300",
-  "text-blue-300",
-  // "text-indigo-300",
   "text-violet-300",
-  // "text-purple-300",
-  // "text-fuchsia-300",
-  "text-rose-300",
+  "text-lime-300",
+  "text-cyan-300",
+
+  "text-orange-300",
+  "text-teal-300",
+  "text-sky-300",
+
+  "text-amber-300",
+  "text-green-300",
+  "text-indigo-300",
+
+  "text-red-300",
+  "text-emerald-300",
+  "text-blue-300",
+
+  "text-purple-300",
+  "text-lime-300",
+  "text-indigo-300",
+
+  "text-fuchsia-300",
+  "text-yellow-300",
+  "text-blue-300",
 ];
 
 let colorIndex = 0;
