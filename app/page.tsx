887bdd1e370915f6bad2bf8cc48e8140fae811dd
Andrew Huth <git@huth.me>
2023-12-26 09:38:58 -0500

    Re-work page

--- app/page.tsx
+++ app/page.tsx
@@ -8,30 +8,23 @@ export default function Home() {
   return (
     <main className="h-screen bg-black font-sans text-lg font-light text-gray-50 sm:pt-6">
       <div className="mx-auto flex max-w-2xl flex-col gap-2">
-        <h1 className={clsx("mb-4 text-3xl", permMark.className)}>
-          Andrew Huth
-        </h1>
-        <p>
-          Frontend software engineer at the{" "}
-          <Link href="https://chanzuckerberg.com/">
-            Chan Zuckerberg Initiative
-          </Link>
-          .
-        </p>
-        <p>
-          See my writing at{" "}
-          <Link href="https://andrewhuth.substack.com">
-            andrewhuth.substack.com
-          </Link>
-          , code at{" "}
-          <Link href="https://github.com/ahuth">github.com/ahuth</Link>, and
-          profile at{" "}
-          <Link href="https://www.linkedin.com/in/andrewhuth2">
-            linkedin.com/in/andrewhuth2
-          </Link>
-          .
-        </p>
-        <p>Random stuff:</p>
+        <h1 className={clsx("text-3xl", permMark.className)}>Andrew Huth</h1>
+        <p>Programmer</p>
+        <ul className="list-inside list-disc">
+          <li>
+            <Link href="https://andrewhuth.substack.com">Writing</Link>
+          </li>
+          <li>
+            <Link href="https://github.com/ahuth">Code</Link>
+          </li>
+          <li>
+            <Link href="https://www.linkedin.com/in/andrewhuth2">LinkedIn</Link>
+          </li>
+          <li>
+            <Link href="mailto:hello@huth.me">Email</Link>
+          </li>
+        </ul>
+        <p>Random stuff</p>
         <ul className="list-inside list-['➝_']">
           <li>
             <Link href="https://ahuth.github.io/raycast/">
