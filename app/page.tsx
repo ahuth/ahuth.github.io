0dc7bca421b2718526d5db05e16d31bfb8b61d6b
Andrew Huth <git@huth.me>
2024-06-21 20:34:22 -0400

    Add a bit more separation between sections

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const permMark = Permanent_Marker({weight: '400', subsets: ['latin']});
 export default function Home() {
   return (
     <main className="h-screen bg-black font-sans text-xl font-light text-gray-50 sm:pt-6">
-      <div className="mx-auto flex max-w-2xl flex-col gap-2">
+      <div className="mx-auto flex max-w-2xl flex-col gap-4">
         <h1 className={clsx('text-4xl', permMark.className)}>Andrew Huth</h1>
         <section>
           <h2>Programmer</h2>
