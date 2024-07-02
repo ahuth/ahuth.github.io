bc96ac5b0ad82863087a76fdff8bc6084717c3c3
Andrew Huth <git@huth.me>
2024-07-02 08:05:20 -0400

    Change tagline once again

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="h-screen bg-black font-sans text-xl font-light text-gray-50 sm:pt-6">
       <div className="mx-auto flex max-w-2xl flex-col gap-4">
         <h1 className={clsx('text-4xl', permMark.className)}>Andrew Huth</h1>
-        <p>Software engineer, focused on React and JavaScript.</p>
+        <p>Frontend software engineer</p>
         <section>
           <h2>Links</h2>
           <ul className="list-inside list-disc">
