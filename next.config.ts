11c65bbd87586690859791279b053bd2062e4afd
Andrew Huth <git@huth.me>
2025-06-23 23:34:49 -0400

    Revert "Try inline css"
    This reverts commit be07960ae8f011a0f244255bd7cbc17808b75f73.

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   poweredByHeader: false,
-  experimental: {
-    inlineCss: true,
-  },
 };
 
 export default nextConfig;
