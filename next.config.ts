be07960ae8f011a0f244255bd7cbc17808b75f73
Andrew Huth <git@huth.me>
2025-06-23 23:31:02 -0400

    Try inline css

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   poweredByHeader: false,
+  experimental: {
+    inlineCss: true,
+  },
 };
 
 export default nextConfig;
